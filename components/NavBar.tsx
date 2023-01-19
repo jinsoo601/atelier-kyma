bf4c93639941f7d7c497a5ee529601de6eba9f1a
Jin Soo Lim <jinsoo601@gmail.com>
2023-01-18 17:08:22 -0800

    added inset to mobile navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -67,7 +67,7 @@ function MobileNav({
   const isSelected = (path: string) => router.pathname.startsWith(path);
   return (
     <div
-      className={`absolute top-0 left-0 h-screen w-screen bg-white transform transition-transform duration-300 ease-in-out ${
+      className={`absolute top-0 left-0 h-screen w-screen inset-0 bg-white transform transition-transform duration-300 ease-in-out ${
         isOpen ? "-translate-x-0" : "-translate-x-full"
       }`}
     >
