d26c1d634e09acf280d75d9e3b129ca3e82800c7
Jinsoo Lim <jinsoo@deliveryrelay.com>
2023-09-03 08:48:58 -0700

    yonghook kim exhibition

---
+++ pages/exhibition/yong-hoon-kim-solo-exhibition/components.tsx
@@ -0,0 +1,70 @@
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
+import type { PortableTextReactComponents } from '@portabletext/react';
+
+import Image from 'next/image';
+import React from 'react';
+import { urlFor } from '../../../lib/sanityClient';
+
+const components: Partial<PortableTextReactComponents> = {
+	types: {
+		image: ({ value }: { value: SanityImageSource }) => {
+			return (
+				<div
+					className={`relative col-span-full mt-4 mx-auto w-full aspect-[3/4] md:w-1/2 md:aspect-square`}
+				>
+					<Image
+						src={urlFor(value).url()}
+						alt="image"
+						fill
+						className="object-contain object-center"
+					/>
+				</div>
+			);
+		},
+	},
+	marks: {
+		em: ({ children }) => <em>{children}</em>,
+	},
+	block: {
+		h1: ({ children }) => (
+			<h1 className="text-3xl font-bold mt-4 mb-2 col-span-full">{children}</h1>
+		),
+		h2: ({ children }) => (
+			<h2 className="text-2xl font-bold mt-4 mb-2 col-span-full">{children}</h2>
+		),
+		h3: ({ children }) => (
+			<h3 className="text-xl font-bold mt-4 mb-2 col-span-full">{children}</h3>
+		),
+		h4: ({ children }) => (
+			<h4 className="text-lg font-bold mt-4 mb-2 col-span-full">{children}</h4>
+		),
+		h5: ({ children }) => (
+			<h5 className="text-base font-bold mt-4 mb-2 col-span-full">
+				{children}
+			</h5>
+		),
+		h6: ({ children }) => (
+			<h6 className="text-sm text-medium mt-2 mb-2 col-span-full text-center">
+				{children}
+			</h6>
+		),
+		blockquote: ({ children }) => (
+			<blockquote className="text-slate-600 italic border-l-4 border-slate-300 pl-4 my-4 text-sm leading-6 col-span-2">
+				{children}
+			</blockquote>
+		),
+		normal: ({ children }) => (
+			<p className="my-4 text-sm leading-6 col-span-full">{children}</p>
+		),
+	},
+	list: {
+		bullet: ({ children }) => <ul className="col-span-full">{children}</ul>,
+	},
+	listItem: {
+		bullet: ({ children }) => (
+			<li className="list-disc list-inside text-sm leading-6">{children}</li>
+		),
+	},
+};
+
+export default components;
---
+++ pages/exhibition/yong-hoon-kim-solo-exhibition/index.tsx
@@ -0,0 +1,49 @@
+import type { GetStaticProps } from 'next';
+import type { ParsedUrlQuery } from 'querystring';
+import type { TypedObject } from '@sanity/types';
+
+import { PortableText } from '@portabletext/react';
+import { client } from '../../../lib/sanityClient';
+import components from './components';
+
+type ExhibitionDetail = {
+	name: string;
+	startDate: string;
+	endDate: string;
+	description: TypedObject[];
+};
+
+type Props = {
+	exhibitionDetail: ExhibitionDetail;
+};
+
+type Params = ParsedUrlQuery & {
+	slug: string;
+};
+
+export default function ExhibitionDetail({ exhibitionDetail }: Props) {
+	return (
+		<>
+			<header className="text-xl font-semibold">{exhibitionDetail.name}</header>
+			<div className="grid grid-cols-1 md:grid-cols-4 gap-x-4">
+				<PortableText
+					value={exhibitionDetail.description}
+					components={components}
+				/>
+			</div>
+		</>
+	);
+}
+
+export const getStaticProps: GetStaticProps<Props, Params> = async ({
+	params,
+}) => {
+	const query = `*[_type == "exhibition" && slug.current == "yong-hoon-kim-solo-exhibition"][0]{name, startDate, endDate, description}`;
+	const exhibitionDetail: ExhibitionDetail = await client.fetch(query);
+
+	return {
+		props: {
+			exhibitionDetail,
+		},
+	};
+};
