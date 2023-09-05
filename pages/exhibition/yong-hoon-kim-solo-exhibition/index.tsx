db217e61bd58abc9933324e409a80e417d7c09c9
Jinsoo Lim <jinsoo@deliveryrelay.com>
2023-09-05 11:02:55 -0700

    yonghoon kim exhibition tweak 2

--- pages/exhibition/yong-hoon-kim-solo-exhibition/index.tsx
+++ pages/exhibition/yong-hoon-kim-solo-exhibition/index.tsx
@@ -24,7 +24,9 @@ type Params = ParsedUrlQuery & {
 export default function ExhibitionDetail({ exhibitionDetail }: Props) {
 	return (
 		<>
-			<header className="text-xl font-semibold">{exhibitionDetail.name.replace('\\n', '\n')}</header>
+			<header className="text-xl font-semibold whitespace-pre-line">
+				{exhibitionDetail.name.replace('\\n', '\n')}
+			</header>
 			<div className="grid grid-cols-1 md:grid-cols-4 gap-x-4">
 				<PortableText
 					value={exhibitionDetail.description}
