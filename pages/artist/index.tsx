b7fd7945d1867a8bd4d2d093e273ccc6c9596ba6
Jinsoo Lim <jinsoo@deliveryrelay.com>
2023-10-20 10:04:55 -0700

    change artist page layout

--- pages/artist/index.tsx
+++ pages/artist/index.tsx
@@ -1,53 +1,54 @@
-import type { GetStaticProps } from "next/types";
-import type { Image as SanityImage, Slug } from "@sanity/types";
+import type { GetStaticProps } from 'next/types';
+import type { Image as SanityImage, Slug } from '@sanity/types';
 
-import { client, urlFor } from "../../lib/sanityClient";
-import Link from "next/link";
-import Image from "next/image";
-import Heading from "@/components/Heading";
+import { client, urlFor } from '../../lib/sanityClient';
+import Link from 'next/link';
+import Image from 'next/image';
+import Heading from '@/components/Heading';
 
 type Artist = {
-  name: string;
-  slug: Slug;
-  profileImage: SanityImage;
+	name: string;
+	slug: Slug;
+	profileImage: SanityImage;
 };
 
 type Props = {
-  artists: Artist[];
+	artists: Artist[];
 };
 
 export default function Artist({ artists }: Props) {
-  return (
-    <>
-      <Heading value="ARTISTS" />
-      <div className=" grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
-        {artists.map(({ name, slug, profileImage }) => (
-          <Link
-            key={slug.current}
-            href={`/artist/${slug.current}`}
-            className="flex flex-col items-center transition-transform hover:scale-105 duration-500"
-          >
-            <Image
-              src={urlFor(profileImage).width(200).height(250).url()}
-              alt={name}
-              width={200}
-              height={250}
-            />
-            <p className="text-sm font-semibold mt-2">{name}</p>
-          </Link>
-        ))}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Heading value="ARTISTS" />
+			<div className=" grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-20">
+				{artists.map(({ name, slug, profileImage }) => (
+					<Link
+						key={slug.current}
+						href={`/artist/${slug.current}`}
+						className="flex flex-col items-center transition-transform hover:scale-105 duration-500"
+					>
+						<div className="relative w-full aspect-[3/4]">
+							<Image
+								src={urlFor(profileImage).width(200).height(250).url()}
+								alt={name}
+								fill
+							/>
+						</div>
+						<p className="text-sm font-semibold mt-2">{name}</p>
+					</Link>
+				))}
+			</div>
+		</>
+	);
 }
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-  const query = '*[_type == "artist"]{name, slug, profileImage}';
-  const artists: Artist[] = await client.fetch(query);
+	const query = '*[_type == "artist"]{name, slug, profileImage}';
+	const artists: Artist[] = await client.fetch(query);
 
-  return {
-    props: {
-      artists,
-    },
-  };
+	return {
+		props: {
+			artists,
+		},
+	};
 };
